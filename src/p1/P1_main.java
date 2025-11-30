9c6cd36757197fe2a32d24aba306baa6f7084a96
Jan Buntenbroich <jan@buntenbroich.de>
2025-11-30 11:18:49 +0100

    p1 fertig

---
+++ src/p1/P1_main.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class P1_main {
+	public static void main(String[] args) {
+		int Input = -3;
+		int squared = squared(Input);
+		System.out.print(squared);
+	}
+	
+	public static int squared(int squared){
+		return (squared + 1) * (squared + 1);
+	}			
+}
