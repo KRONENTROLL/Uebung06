c4134181d7a48b730538f497784db5b194991d21
Jan Buntenbroich <jan@buntenbroich.de>
2025-11-30 19:52:49 +0100

    p3 fertig

---
+++ src/p3/P3_main.java
@@ -0,0 +1,18 @@
+package p3;
+public class P3_main {
+public static void main(String[] args) {
+}
+	public static int methode(int[] a) {
+		int max =-1;
+		int k = a.length- 1;
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j < k; j++) {
+				if (a[j] > a[i]) {
+					max = a[j];
+					
+				}
+			}
+		}
+		return max;	
+	}
+}
